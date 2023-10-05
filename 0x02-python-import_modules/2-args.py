6a9db1e0c8e79a2da4e78c13f5782c6c68e4724f
JonaNdawula <jonathan.ndawula@yahoo.com>
2023-10-05 00:59:57 -0700

    First Three solutions

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    item = len(sys.argv) - 1
+    if item == 0:
+        print("0 arguments.")
+    elif item == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(item))
+    for index in range(item):
+        print("{}:{}".format(index + 1, sys.argv[index + 1]))
