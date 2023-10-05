908986ff80c5513d01ec6e0ec3dc542d7bdfaac2
JonaNdawula <jonathan.ndawula@yahoo.com>
2023-10-05 05:24:43 -0700

    the Python function def magic_calculation(a, b):

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
+def magic_calculation(a, b):
 
+    from magic_calculation_102 import add, sub
+    if a < b:
+        add_new = add(a, b)
+        for index in range(4, 6):
+            add_new = add(add_new, index)
+        return (add_new)
+    else:
+        retrun(sub(a, b))
