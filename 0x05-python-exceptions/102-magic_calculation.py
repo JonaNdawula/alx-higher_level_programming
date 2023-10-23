2e6ef94f00237023272d54635d73d416be8ce971
JonaNdawula <jonathan.ndawula@yahoo.com>
2023-10-23 08:56:43 -0700

    Python function def magic_calculation(a, b):

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    res = 0
+
+    for index in range(1, 4):
+        try:
+            if index > a:
+                raise Exception('Too far')
+            res += (a ** b) / index
+        except Exception:
+            pass
+    res += a + b
+    return (res)
