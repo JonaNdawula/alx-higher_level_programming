43926fe1d9dbc54eb7a53dcfa1ba309dbfbbf135
JonaNdawula <jonathan.ndawula@yahoo.com>
2023-10-24 09:12:38 -0700

    a class Square that defines a square by: (based on 5-square.py)

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -50,7 +50,6 @@ class Square:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
-        else:
             self.__size = value
 
     @property
