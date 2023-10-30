7a1f4770c9357f2abfbd72fc2b7d5c37345ca43d
JonaNdawula <jonathan.ndawula@yahoo.com>
2023-10-30 11:29:06 -0700

    a class Rectangle that defines a rectangle by: (based on 6-rectangle.py)

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -83,7 +83,7 @@ class Rectangle:
         if self.__height == 0 or self.__width == 0:
             return (0)
         else:
-            return (2 * (self.__height + self._width))
+            return (2 * (self.__height + self.__width))
 
     def __str__(self):
         """
