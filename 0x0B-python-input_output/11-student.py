f6f9c1c5544969c62517549c0a851c7766ea5293
JonaNdawula <jonathan.ndawula@yahoo.com>
2023-11-07 04:07:05 -0800

    A  class Student that defines a student

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Class Student
+"""
+
+
+class Student:
+    """
+    A class Student that defines a student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        creates new student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Json for a student
+        """
+        if attrs is None:
+            return (self.__dict__)
+        my_dict = {}
+        i = 0
+
+        while i < len(attrs):
+            item = attrs[i]
+
+            try:
+                my_dict[item] = self.__dict__[item]
+            except Exception:
+                pass
+            i += 1
+
+    def reload_from_json(self, json):
+        """
+         Replaces all attributes of the Student instance
+        """
+
+        self.__dict.update(json)
