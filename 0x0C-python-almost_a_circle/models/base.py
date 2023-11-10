bcc59991749a2ffd5c82ade61d52237f479644ae
JonaNdawula <jonathan.ndawula@yahoo.com>
2023-11-10 10:53:55 -0800

    updated base class by adding sttic method to_json_string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """
 Base class
 """
+import json
 
 
 class Base:
@@ -20,3 +21,15 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Returns the JSON string
+        representation of list_dictionaries
+        """
+
+        if list_dictionaries is None or list_dictionaries == "[]":
+            return ("[]")
+        else:
+            return (json.dumps(list_dictionaries))
