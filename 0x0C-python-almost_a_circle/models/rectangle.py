bce515b9ee86eed7bfc9f883d913b08e2261575d
JonaNdawula <jonathan.ndawula@yahoo.com>
2023-11-10 10:29:45 -0800

    Update to the class Rectangle, added def to_dictionary(self)

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -153,3 +153,16 @@ class Rectangle(Base):
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        """
+        Returns the dictionary
+        representation of a Rectangle
+        """
+        rect_dict = {}
+        rect_dict["id"] = self.id
+        rect_dict["width"] = self.width
+        rect_dict["height"] = self.height
+        rect_dict["x"] = self.x
+        rect_dict["y"] = self.y
+        return (rect_dict)
