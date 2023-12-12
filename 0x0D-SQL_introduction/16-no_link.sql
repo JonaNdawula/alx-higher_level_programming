d1b04ed7a3a74b4fbb86e8d952a1074924b0fc01
JonaNdawula <jonathan.ndawula@yahoo.com>
2023-12-12 11:43:31 -0800

    A script that lists all records of the table second_table of the database hbtn_0c_0

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,4 @@
 -- script 16
 -- A script that lists all records of the 
 -- table second_table of the database hbtn_0c_0
-SELECT score, mname FROM second_table WHERE name IS NOT NULL ORDER BY score DESC
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC
