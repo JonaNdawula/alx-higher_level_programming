13b919a33c4919eba8fc09970770a316d8c63a36
JonaNdawula <jonathan.ndawula@yahoo.com>
2023-12-13 03:15:07 -0800

    A script that lists all cities contained in the database hbtn_0d_usa.

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,3 @@
+-- script 9
+-- A script that lists all cities contained in the database hbtn_0d_usa.
+SELECT cities.id, cities.name, states.name from cities INNER JOIN states ON cities.state_id=states.id ORDER BY cities.id
