b56620d3415926f57d9a07b775f10cc301af1fa2
JonaNdawula <jonathan.ndawula@yahoo.com>
2024-01-08 06:29:01 -0800

    update for 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,9 +2,8 @@
 
 const argv = process.argv;
 let i = parseInt(argv[2], 10);
-
 if (typeof i === 'number') {
-  for (; i > 0; i--) {
+  for ( ; i > 0; i--) {
     console.log('C is fun');	  
   }
 } else console.log('Missing number of occurances');
