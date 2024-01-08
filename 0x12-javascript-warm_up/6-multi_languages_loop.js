431cc2fe58153f9b0008a68d4803d79406654517
JonaNdawula <jonathan.ndawula@yahoo.com>
2024-01-08 04:43:51 -0800

    update for 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,8 +2,8 @@
 
 const myString = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let i = 0;
-while (i < myString.length)
-{
+
+while (i < myString.length) {
 	console.log(myString[i]);
 	i++;
 }
