9d214877ba92036756fa2a47b4a081c3740235f5
JonaNdawula <jonathan.ndawula@yahoo.com>
2024-01-08 07:27:02 -0800

    update for 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,7 +2,7 @@
 const argv = process.argv;
 const varOne = parseInt(argv[2], 10);
 const varTwo = parseInt(argv[3], 10);
-function add(a, b) {
+function add (a, b) {
   return a + b;
 }
 console.log(add(varOne, varTwo));
