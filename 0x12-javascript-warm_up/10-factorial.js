6b2a7212e0e7d478eaff062a1b9edcc1901485d6
JonaNdawula <jonathan.ndawula@yahoo.com>
2024-01-08 08:04:10 -0800

    updte for 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,9 +2,7 @@
 const argv = process.argv;
 const num = parseInt(argv[2], 10);
 function fact (num) {
-  if (num === 0 || num === 1) {
-    return 1;
-}
+  if (num === 0 || num === 1) return 1;
   return num * fact(num - 1);
 }
 if (isNaN(num)) console.log(1);
