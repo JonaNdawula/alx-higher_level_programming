fa0e6771034add76bf8bf0f06271461631a45a95
JonaNdawula <jonathan.ndawula@yahoo.com>
2024-01-09 06:58:34 -0800

    update for 7-occurrences.js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -4,9 +4,7 @@ exports.nbOccurences = function (list, searchElement) {
   let occurrenceCount = 0;
   let x = 0;
   while (x < list.length) {
-    if (list[x] === searchElement) {
-	    occurrenceCount++;
-  }
+    if (list[x] === searchElement) { occurrenceCount++; }
     x++;
   }
   return (occurrenceCount);
