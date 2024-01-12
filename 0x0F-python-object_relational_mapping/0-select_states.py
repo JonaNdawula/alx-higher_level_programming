71b4455346d32218bb71097e5e9a989c6593645e
JonaNdawula <jonathan.ndawula@yahoo.com>
2024-01-12 01:29:56 -0800

    code for 0-select_states.py

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+States models
+"""
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    mysql_host = "localhost"
+    mysql_user = sys.argv[1]
+    mysql_password = sys.argv[2]
+    mysql_name = sys.argv[3]
+    port = 3306
+
+    data_base = MySQLdb.connect(
+        host=mysql_host, user=mysql_user, password=mysql_password,
+        database=mysql_name, port=port)
+    cursor = data_base.cursor()
+
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    rws = cursor.fetchall()
+
+    for rw in rws:
+        print(rw)
+
+    cursor.close()
+    data_base.close()
