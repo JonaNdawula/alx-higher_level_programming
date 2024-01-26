bda075f79e7048734183e0b779bfd0b14cab8a07
JonaNdawula <jonathan.ndawula@yahoo.com>
2024-01-26 05:34:48 -0800

    code for 10-my_github.py

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+A Python script that takes your GitHub credentials
+(username and password) and uses the GitHub
+API to display your id
+"""
+import sys
+from requests.auth import HTTPBasicAuth
+import requests
+
+if __name__ == "__main__":
+    myurl = "https://api/github.com/user"
+    username = sys.argv[1]
+    password = sys.argv[2]
+    res = requests.get(myurl, auth=HTTPBasicAuth(username, password))
+    json_res = res.json()
+    myjson = json_res.get("id")
+    print(f"{myjson}")
