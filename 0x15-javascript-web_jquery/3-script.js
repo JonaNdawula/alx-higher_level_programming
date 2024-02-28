59eb6f98cc617b0b2483df1945112e8cb5a6929d
JonaNdawula <jonathan.ndawula@yahoo.com>
2024-02-28 04:53:48 -0800

    update for 3-script.js

--- 0x15-javascript-web_jquery/3-script.js
+++ 0x15-javascript-web_jquery/3-script.js
@@ -1,6 +1,6 @@
-// jQuery API to JavaScript script that adds the class red to 
+// jQuery API to JavaScript script that adds the class red to
 // the <header> element when the user clicks on the tag DIV#red_header
 
 $('div#red_header').click(function () {
-    $('header').addClass('red');
-  });
+  $('header').addClass('red');
+});
