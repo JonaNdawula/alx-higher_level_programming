76a056b37650d6fc7a528b495c4b7e42155ccffe
JonaNdawula <jonathan.ndawula@yahoo.com>
2024-02-28 04:50:34 -0800

    debug for 2-script.js

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,5 +1,5 @@
 // jQuery API that updates text color of header to red
 
-$('div#red_header').click(function() {
-    $('header').css('color', '#FF0000');
-  });
+$('div#red_header').click(function () {
+  $('header').css('color', '#FF0000');
+});
